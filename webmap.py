bb6c1f2d13cd7ec91d581656122510c1d9e9b7d1
Antoine Maranda <antoine.maranda@ec.gc.ca>
2023-08-14 16:12:48 -0400

    take 2

--- webmap.py
+++ webmap.py
@@ -102,7 +102,8 @@ st.markdown(streamlit_style, unsafe_allow_html=True)
 
 #repo_link='https://007gc-my.sharepoint.com/personal/antoine_maranda_ec_gc_ca/_layouts/15/onedrive.aspx?login_hint=antoine%2Emaranda%40ec%2Egc%2Eca&id=%2Fpersonal%2Fantoine%5Fmaranda%5Fec%5Fgc%5Fca%2FDocuments%2FDEM%5FGLAM%2FDEM%5F10m'
 #st.title(f"PRELIMINARY VERSION of Topographic and bathymetric Digital Terrain Model\n Lake Ontario and Upper St. Lawrence River [Download Repository (ECCC members only)]({repo_link})")
-repo_link='https://007gc-my.sharepoint.com/:f:/r/personal/antoine_maranda_ec_gc_ca/Documents/DEM_GLAM/DTM_LKO_USL_CAN_v4?csf=1&web=1&e=4tdin7'
+#repo_link='https://007gc-my.sharepoint.com/:f:/r/personal/antoine_maranda_ec_gc_ca/Documents/DEM_GLAM/DTM_LKO_USL_CAN_v4?csf=1&web=1&e=4tdin7'
+repo_link='https://007gc-my.sharepoint.com/personal/antoine_maranda_ec_gc_ca/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fantoine%5Fmaranda%5Fec%5Fgc%5Fca%2FDocuments%2FDEM%5FGLAM%2FDTM%5FLKO%5FUSL%5Fv4&view=0'
 #repo_link='https://007gc-my.sharepoint.com/:f:/g/personal/antoine_maranda_ec_gc_ca/Er5RUAKOWQROpjumUgyyPg4Bv10ZLSsNg-mUpcsBviUlOg?e=57q7Y3'
 plotly_link='https://007gc-my.sharepoint.com/:f:/g/personal/antoine_maranda_ec_gc_ca/EmYSvAyqCv9MnqSQw2Ic9BEB0AYRx9XJYZkeWZMUzLdiXg?e=0WdUCJ'
 st.title(f"PRELIMINARY VERSION of Topographic and bathymetric Digital Terrain Model (Lake Ontario and Upper St. Lawrence River)")
