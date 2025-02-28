85badb8ee4cd6f6c0998a87a6c37a343c0da09d3
Antoine Maranda <65919793+eccc-Antoine@users.noreply.github.com>
2025-02-28 11:58:18 -0500

    Update webmap.py

--- webmap.py
+++ webmap.py
@@ -108,7 +108,7 @@ repo_link='https://ijccmi.sharepoint.com/sites/GLAM/ILOSLRB%20Technical%20Teams/
 #repo_link='https://007gc-my.sharepoint.com/:f:/g/personal/antoine_maranda_ec_gc_ca/Er5RUAKOWQROpjumUgyyPg4Bv10ZLSsNg-mUpcsBviUlOg?e=57q7Y3'
 plotly_link='https://007gc-my.sharepoint.com/:f:/g/personal/antoine_maranda_ec_gc_ca/EmYSvAyqCv9MnqSQw2Ic9BEB0AYRx9XJYZkeWZMUzLdiXg?e=0WdUCJ'
 st.title(f"Version 4.2 of Great Lake St. Lawrence River Topo-Bathymetric Digital Elevation Model (GLSLR-DEM)")
-members='(Expedited Review working grou members only)'
+members='(Expedited Review working group members only)'
 st.header(f'[Full resolution DTM Download Repository {members}]({repo_link})')
 st.subheader('Or click on a tile for a 1m resolution overview')
 #link_mosaic='https://007gc-my.sharepoint.com/personal/antoine_maranda_ec_gc_ca/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fantoine%5Fmaranda%5Fec%5Fgc%5Fca%2FDocuments%2FDEM%5FGLAM%2FDEM%5F10m%2Fmosaic%5F100m%5F4326%2Ezip&parent=%2Fpersonal%2Fantoine%5Fmaranda%5Fec%5Fgc%5Fca%2FDocuments%2FDEM%5FGLAM%2FDEM%5F10m'
